79208ae02b0e91634db12c80e84492d1ca882989
Anton9512 <anton.mail9512@gmail.com>
2022-06-23 11:09:27 +0300

    add postItem

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import PostItem from "./components/PostItem";
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("ТЕКСТ В ИНПУТЕ");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PostItem post={{ id: 1, title: "JS1", body: "Description1" }} />
     </div>
   );
 }
