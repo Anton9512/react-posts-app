47a322ed38557db75d6b2223cd6a8e3ea142e5c8
Anton9512 <anton.mail9512@gmail.com>
2022-06-30 17:07:19 +0300

    add usePosts and useEffect

--- src/App.js
+++ src/App.js
@@ -1,62 +1,35 @@
-import { useMemo, useState } from "react";
+import axios from "axios";
+import { useEffect, useMemo, useState } from "react";
 import "./App.css";
 import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyModal from "./components/UI/modal/MyModal";
+import { usePosts } from "./hooks/usePosts";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "aa",
-      body: "бб",
-    },
-    {
-      id: 2,
-      title: "bb",
-      body: "аа",
-    },
-    {
-      id: 3,
-      title: "cc",
-      body: "гг",
-    },
-    {
-      id: 4,
-      title: "нн",
-      body: "лл",
-    },
-    {
-      id: 5,
-      title: "дд",
-      body: "мм",
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
 
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
-
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((post1, post2) =>
-        post1[filter.sort].localeCompare(post2[filter.sort])
-      );
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(filter.query.toLowerCase())
-    );
-  }, [filter.query, sortedPosts]);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
   };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  async function fetchPosts() {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    setPosts(response.data);
+  }
 
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
