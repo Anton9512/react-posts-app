8abaf74707ab925132d96374f9543f5848514c3e
Anton9512 <anton.mail9512@gmail.com>
2022-07-25 12:45:55 +0300

    useContext, Auth

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { Route, Routes,Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { AuthContext } from "../context";
 import { privateRoutes, publicRoutes } from "../router/index";
+import Loader from "./UI/Loader/Loader";
 
 const AppRouter = () => {
-  
+  const { isAuth, isLoading } = useContext(AuthContext);
 
+  if (isLoading) {
+    return <Loader />;
+  }
   return isAuth ? (
     <Routes>
       {privateRoutes.map((route) => (
